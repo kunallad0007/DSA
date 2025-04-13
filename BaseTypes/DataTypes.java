4fd55abb2fead8d0c824b9de9edf77a03b654b18
kunallad0007 <kunallad0007@gmail.com>
2025-04-13 19:54:57 +0530

    Add Data types file

---
+++ BaseTypes/DataTypes.java
@@ -0,0 +1,30 @@
+package BaseTypes;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        
+        boolean flag = true;                 // ek boolean variable
+        char grade = 'A';                    // ek character variable
+        byte b = 12;                         // byte type variable
+        short s = 24;                        // short type variable
+        int k = 257;                   // teen int, sirf k initialized hai
+        long l = 890L;                       // long ke liye L lagaya
+        float pi = 3.1416F;                  // float ke liye F lagaya
+        double e = 2.71828, a = 6.022e23;    // do double type variables
+
+        System.out.println("Boolean flag: " + flag);
+        System.out.println("Character grade: " + grade);
+        System.out.println("Byte b: " + b);
+        System.out.println("Short s: " + s);
+        System.out.println("Integer k: " + k);
+        System.out.println("Long l: " + l);
+        System.out.println("Float pi: " + pi);
+        System.out.println("Double e: " + e);
+        System.out.println("Double a: " + a);
+
+
+    }
+
+    
+}
